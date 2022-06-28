2d5c7968e6f2a824802e226c6fb10e500af2c3f3
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2022-06-29 00:20:50 +0530

    added links to icons

--- know-shailesh/src/components/Footer.js
+++ know-shailesh/src/components/Footer.js
@@ -5,25 +5,42 @@ import Twitter from "./twitter.png";
 import Linkedin from "./linkedin.png";
 import Hashnode from "./hashnode.png";
 import Rectangle from "./reactangle.png";
+import { NavLink, Link } from "react-router-dom";
 function Footer() {
   return (
     <>
       <div className="footer__container">
         <div className="">
-            <div className="footer__name__container">
+          <div className="footer__name__container">
             <img className="rectangle" src={Rectangle} />
-          <div className="footer__name"> Shailesh Pandey</div>
-            </div>
-         
+            <div className="footer__name"> Shailesh Pandey</div>
+          </div>
 
           <div className="footer__rights">
             @2022 Shailesh Pandey. All Rights Reserved{" "}
           </div>
           <div className="icons__footer">
-            <img className="icons" src={Linkedin}></img>
-            <img className="icons" src={Twitter}></img>
-            <img className="icons" src={Github}></img>
-            <img className="icons" src={Hashnode}></img>
+            <a
+              href="
+            https://www.linkedin.com/in/shailesh-pandey-980448201/
+            "
+              target="blank"
+            >
+              <img className="icons" src={Linkedin}></img>
+            </a>
+            <a href="https://twitter.com/theshailesh_" target="blank">
+              <img className="icons" src={Twitter}></img>
+            </a>
+
+            <a
+              href="https://github.com/ShaileshDeveloper?tab=repositories"
+              target="blank"
+            >
+              <img className="icons" src={Github}></img>
+            </a>
+            <a href="https://shailesh-blogs.hashnode.dev/" target="blank">
+              <img className="icons" src={Hashnode}></img>
+            </a>
           </div>
         </div>
       </div>
