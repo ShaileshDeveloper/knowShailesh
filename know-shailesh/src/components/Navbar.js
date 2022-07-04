65d98b7409dbc00ee42df9db3d9236107da1ea40
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2022-07-04 12:13:40 +0530

    fixed navbar mobile

--- know-shailesh/src/components/Navbar.js
+++ know-shailesh/src/components/Navbar.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Rectangle from "./reactangle.png";
 import "./Navbar.css";
-import Hamburger from "./hamburger.png"
+import Hamburger from "./hamburger.png";
+import Cross from "./cross.png";
 function Navbar() {
+  const [clicked, setClicked] = useState(false);
   return (
     <>
       <div className="navbar__container">
         <div className="logo__container">
-          <img className="rectangle__icon" src={Rectangle}/>
-          <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-          Shailesh Pandey
+          <img className="rectangle__icon" src={Rectangle} />
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Shailesh Pandey
           </div>
         </div>
         <div className="nav__list__items">
@@ -17,8 +25,33 @@ function Navbar() {
           <div className="home__container">PROJECTS</div>
           <div className="home__container">BLOGS</div>
         </div>
-        <div className="hamburger__icon">
-              <img src={Hamburger} alt="menu"/>
+        <div className="hamburger__icon" onClick={() => setClicked(true)}>
+          <img src={Hamburger} alt="menu" />
+        </div>
+      </div>
+      <div
+        style={{
+          transition: clicked ? "0.3s ease" : "0.3s ease",
+          height: clicked ? "280px" : "0px",
+          opacity: clicked ? "1" : "0",
+          overflow: clicked ? "visible" : "hidden",
+        }}
+        className="mobile__navbar"
+      >
+        <div
+          style={{
+            position: "absolute",
+            right: "15px",
+            top: "10px",
+          }}
+          onClick={() => setClicked(false)}
+        >
+          <img style={{ width: "40px", height: "40px" }} src={Cross} alt="" />
+        </div>
+        <div>
+          <div className="mobile__navbar__items">HOME</div>
+          <div className="mobile__navbar__items">PROJECTS</div>
+          <div className="mobile__navbar__items">BLOGS</div>
         </div>
       </div>
     </>
@@ -26,3 +59,30 @@ function Navbar() {
 }
 
 export default Navbar;
+
+// function Navbar() {
+//   return (
+
+//     <div>
+//       <div class="navbar">
+//         <div class="container nav-container">
+//             <input class="checkbox" type="checkbox" name="" id="" />
+//             <div class="hamburger-lines">
+//               <span class="line line1"></span>
+//               <span class="line line2"></span>
+//               <span class="line line3"></span>
+//             </div>
+//
+//           <div class="menu-items">
+//             <li><a href="#">Home</a></li>
+//             <li><a href="#">about</a></li>
+//             <li><a href="#">blogs</a></li>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+
+//   )
+// }
+
+// export default Navbar
